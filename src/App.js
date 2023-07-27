a93e7d070f618810f92705dbdc1d997d8d30c1b2
Fllyer <139190216+Fllyer-dev@users.noreply.github.com>
2023-07-27 05:07:13 +0300

    update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
 import Counter from './components/Counter.jsx';
+import Input from './components/Input.jsx';
 
 function App() {
   return (
     <div className="App">
     <Counter />
+    <Input />
     </div>
   );
 }
