87ff9425aa476edc6bd5db36e0b3c5264bfc1c2a
Fllyer <139190216+Fllyer-dev@users.noreply.github.com>
2023-07-27 05:06:32 +0300

    UPD

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,1 +1,17 @@
+import React, {useState} from 'react';
 
+function Input() {
+  const[value, setValue] = useState('Enter');
+  return (
+    <div>
+    <h1>{value}</h1>
+    <input
+     type='text'
+     value={value}
+     onChange={event => setValue(event.target.value)}
+     />
+    </div>
+    );
+}
+
+export default Input;
