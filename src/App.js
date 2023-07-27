cb02c7e4e6ccd1622f571c71513a3b346fc15cf3
Fllyer <139190216+Fllyer-dev@users.noreply.github.com>
2023-07-27 04:52:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
+import Counter from './components/Counter.jsx';
+
 function App() {
-  const [star, setStar] = useState(5);
-  const [value, setValue] = useState('');
-  
-  function starPlus() {
-    setStar(star + 1);
-  }
   return (
     <div className="App">
-    <h1>{value}</h1>
-    <input 
-    type='text'
-    value={value}
-    onChange={event => setValue(event.target.value)}
-    />
-    <h1>{star}</h1>
-    <button onClick={starPlus}>Click</button>
+    <Counter />
     </div>
   );
 }
