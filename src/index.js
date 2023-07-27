3ab35376bbafe1a0a101420e28b03bd33a279c5f
Fllyer <139190216+Fllyer-dev@users.noreply.github.com>
2023-07-27 03:30:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>Hello world</div>
+   <App /> // Читай App.js
+  // <div>Hello world</div>
   </React.StrictMode>
 );
